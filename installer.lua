4dab8c037d96962a0d01b3750fe957dfe9021419
Alexander Horner <33007665+alexhorner@users.noreply.github.com>
2020-08-16 21:19:08 +0100

    oops

--- installer.lua
+++ installer.lua
@@ -1,5 +1,5 @@
 shell.run("wget https://raw.githubusercontent.com/alexhorner/EnterpriseClient/master/startup.lua /startup.lua");
 
-shell.run("wget https://raw.githubusercontent.com/alexhorner/EnterpriseClient/master/drivers/display.lua /system/drivers/display.lua");
+shell.run("wget https://raw.githubusercontent.com/alexhorner/EnterpriseClient/master/system/drivers/display.lua /system/drivers/display.lua");
 
 os.reboot();
